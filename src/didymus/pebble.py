16e318735da31c8aba741b6dbff3d1efbd2caad7
Zoe R <zoerichter96@gmail.com>
2024-02-21 12:24:29 -0600

    added docstrings

--- src/didymus/pebble.py
+++ src/didymus/pebble.py
@@ -1,14 +1,36 @@
-#imports
+# imports
 
 class Pebble:
-	'''
-	Contains data on a single pebble
-	'''
-	
-	def __init__(self,coords,rad,mat_id,uniq_id,recirc=False):
-		self.coords = coords
-		self.rad = rad
-		self.mat_id = mat_id
-		self.uniq_id = uniq_id
-		self.recirc = recirc
-	
+    '''
+    Class for a single pebble object.  Contains information
+    necessary to determine the pebble location within the core
+    and track its material composition and unique id number.
+    '''
+
+    def __init__(self, coords, rad, mat_id, uniq_id, recirc=False):
+        '''
+        Initializes a single instance of a Pebble object.
+
+        Parameters
+		----------
+
+		coords : array
+            Array containing the x, y, and z coordinates of the pebble.
+        rad : float
+            Radius of the pebble, with the same units as core measurements.
+        mat_id : int or str
+            User-defined integer or string for the material in the pebble,
+            which multiple pebble objects can share.
+        uniq_id : int
+            User-defined integer for identifying a specific pebble.
+            Each uniq_id should be distinct.
+        recirc : bool
+            Boolean for determining if a given pebble should recirculate,
+            for use in multi-pass cycles.  Defaults to False.
+
+        '''
+        self.coords = coords
+        self.rad = rad
+        self.mat_id = mat_id
+        self.uniq_id = uniq_id
+        self.recirc = recirc
--- src/didymus/reader.py
+++ src/didymus/reader.py
@@ -1,20 +1,62 @@
-#import here
+# import here
 import numpy as np
 from didymus import pebble as peb
 
 
-#defining code-specific readers here:
+# defining code-specific readers here:
 class OpenmcReader():
-	"""
-	Class for reading Openmc pebble placement
-	"""
-	
-	def __init__(self,coord_array):
-		self.coord_array = coord_array
-	
-	def pebs(self,peb_rad,mat_ids,uniq_ids):
-		peb_array = []
-		for i, coord, in enumerate(self.coord_array):
-			peb_array.append(peb.Pebble(coord,peb_rad,mat_ids[i],uniq_ids[i]))
-			
-		return peb_array
+    """
+    Class for reading Openmc pebble coordinates from the
+    OpenMC pack_spheres() function into and arrary of
+    distinct Pebble objects.
+    """
+
+    def __init__(self, coord_array):
+        '''
+        Initializes the OpenmcReader
+
+        Parameters
+		----------
+		coord_array : array
+			Array of center coordinates, in x, y, and z, from
+            the pack_spheres() function of OpenMC.
+
+        '''
+        self.coord_array = coord_array
+
+    def pebs(self, peb_rad, mat_ids, uniq_ids):
+        '''
+        Creates an array of didymus Pebble objects, using
+        the central coordinates in coord_array.
+
+        Parameters
+		----------
+		peb_rad : float
+			Radius of a single pebble, with units matching those
+            used to create center coordinates.  Assumes all
+            pebbles are the same size.
+        mat_ids : array of int or str
+            Array containing the mat_ids associated with each pebble
+            center.  While multiple pebbles can share the same mat_id,
+            the length of the mat_ids array should match coord_array.
+        uniq_ids : array of int
+            Array containing the uniq_ids associated with each pebble
+            center.  Each pebble should have a distinct uniq_id.
+
+		Returns
+		----------
+		peb_array : array of Pebble objects
+			Array containing a unique Pebble object for each
+            coordinate provided in coord_array.
+
+        '''
+        peb_array = []
+        for i, coord, in enumerate(self.coord_array):
+            peb_array.append(
+                peb.Pebble(
+                    coord,
+                    peb_rad,
+                    mat_ids[i],
+                    uniq_ids[i]))
+
+        return peb_array
