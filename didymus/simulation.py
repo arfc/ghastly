953f9b830e5bfd4d7717db325321edc48e22f6d8
Zoe R <zoerichter96@gmail.com>
2024-09-11 13:19:44 -0500

    adding sim class to hold simulation-wide attributes, will add more as needed

---
+++ didymus/simulation.py
@@ -0,0 +1,11 @@
+class Sim:
+    '''
+    Class for containing simulation-wide parameters and methods.
+    '''
+    def __init__(self, r_pebble, t_final, down_flow = True):
+        '''
+        Initializes the Sim class.
+        '''
+        self.r_pebble = r_pebble
+        self.t_final = t_final
+        self.down_flow = down_flow
